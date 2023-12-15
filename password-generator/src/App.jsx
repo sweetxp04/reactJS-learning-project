40358034d9d83e3bfabfbd664144be5cd23dd39f
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2023-12-15 23:47:24 +0600

    Passwrod generator initialized

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-white text-center">
+                Hello world!
+            </h1>
+        </>
+    );
+}
+
+export default App;
